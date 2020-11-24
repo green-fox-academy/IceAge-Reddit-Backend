a7232fc839a32900ba9ca03a3137ea5627abfc1b
TomasBenes <tomasbenes123gmail.com>
2020-11-24 14:41:52 +0100

    added find methods to UserRepository

--- src/repositories/UserRepository.ts
+++ src/repositories/UserRepository.ts
@@ -4,8 +4,16 @@ import {User} from "src/models/user";
 @EntityRepository(User)
 export class UserRepository extends Repository<User> {
        
-    findByName() {
-        return this.findOne();
+    findByUserName(username: string) {
+      return this.createQueryBuilder("user")
+          .where("user.username = :username", { username })
+          .getOne();
+    }
+
+    findById (id: number) {
+    return this.createQueryBuilder("user")
+    .where("user.id = :id", { id })
+    .getOne();
     }
 
     saveUser (user: User) {
