0577aa83375461c192d664ca9fded783fffcffd7
Martin Fučík <mar.fucik@gmail.com>
2020-11-27 18:02:22 +0100

    Created Endpoint for log in

--- src/controllers/UserController.ts
+++ src/controllers/UserController.ts
@@ -8,7 +8,12 @@ export class HelloWorldController {
 	constructor(private userService: UserService){}
 
   @Post("/sign-in")
-  create(@BodyParams() user: User) {
+  create(@BodyParams() user: UserCreation) {
     return this.userService.create(user);
+	}
+	
+	@Post("/log-in")
+  logIn(@BodyParams() user: UserLogin) {
+    return this.userService.log(user);
   }
 }
