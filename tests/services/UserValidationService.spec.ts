9562b3a8f12306abbc117a9e6fc7cc33f0ea5a48
Martin Fučík <mar.fucik@gmail.com>
2020-11-29 16:46:58 +0100

    Created tests for strong password check

---
+++ tests/services/UserValidationService.spec.ts
@@ -0,0 +1,37 @@
+/* eslint-disable @typescript-eslint/unbound-method */
+import { Unauthorized } from '@tsed/exceptions';
+import { assert, expect } from "chai";
+import { UserCreation } from '../../src/models/UserCreation';
+import { UserValidationService } from '../../src/services/UserValidationService';
+
+describe("UserValidationService", () => {
+  describe("validateUserCreation()", () => {
+		it("should not fail", () => {
+			const service = new UserValidationService();
+			
+			const userCreation: UserCreation = {
+				username: "Karhal",
+				email: "karhal@email.com",
+				password: "Str0ngPa55word"
+			}
+
+			assert.doesNotThrow(() => {service.validateUserCreation(userCreation)});
+    });
+		it("should fail with Week Password", () => {
+			const service = new UserValidationService();
+			
+			const userCreation: UserCreation = {
+				username: "Karhal",
+				email: "karhal@email.com",
+				password: "1"
+			}
+
+			expect(() => {service.validateUserCreation(userCreation)})
+			.to.throw(Unauthorized, "Week password!");
+
+			userCreation.password = "jfdadfjdk";
+			expect(() => {service.validateUserCreation(userCreation)})
+			.to.throw(Unauthorized, "Week password!"); 
+    });
+  });
+});
