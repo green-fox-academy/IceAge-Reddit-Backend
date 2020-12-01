4d1bbff3a3de290453ee32e4e169b7b15b8dde1c
Martin Fučík <mar.fucik@gmail.com>
2020-12-01 17:31:56 +0100

    Implemented Simons notes

--- tests/services/UserValidationService.spec.ts
+++ tests/services/UserValidationService.spec.ts
@@ -1,98 +1,97 @@
 /* eslint-disable @typescript-eslint/unbound-method */
 import { Unauthorized } from '@tsed/exceptions';
 import { assert, expect } from "chai";
+import { beforeEach } from 'mocha';
 import { UserCreation } from '../../src/models/auth.types';
 import { UserValidationService } from '../../src/services/UserValidationService';
 
 describe("UserValidationService", () => {
 	const service = new UserValidationService();
-  describe("validateUserCreation()", () => {
+	const userCreation: UserCreation = new UserCreation();
+	beforeEach(() => {
+		userCreation.username = "Karhal";
+		userCreation.email = "karhal@email.com";
+		userCreation.password = "Str0ngPa55word";
+	});
+	describe("validateUserCreation()", () => {
 		it("should not fail", () => {	
-			const userCreation: UserCreation = {
-				username: "Karhal",
-				email: "karhal@email.com",
-				password: "Str0ngPa55word"
-			}
-
 			assert.doesNotThrow(() => {service.validateUserCreation(userCreation)});
 		});
-		describe("username property", () => {	
-			const userCreation: UserCreation = {
-				username: "",
-				email: "karhal@email.com",
-				password: "Str0ngPa55word"
-			}
 
+		describe("username property", () => {	
 			it("should fail cause empty username", () => {
+				userCreation.username = "";
+
 				expect(() => {service.validateUserCreation(userCreation)})
 				.to.throw(Unauthorized, "Missing credentials!");
 			});
 
 			it("should fail cause has whitespace", () => {
 				userCreation.username = "Baba Jaga";
+
 				expect(() => {service.validateUserCreation(userCreation)})
 				.to.throw(Unauthorized, "Whitespaces in username!");
 			});
 		});
-		describe("email property", () => {	
-			const userCreation: UserCreation = {
-				username: "Karhal",
-				email: "",
-				password: "Str0ngPa55word"
-			}
 
+		describe("email property", () => {	
 			it("should fail cause empty email", () => {
+				userCreation.email = "";
+
 				expect(() => {service.validateUserCreation(userCreation)})
 				.to.throw(Unauthorized, "Missing credentials!");
 			});
 
 			it("should fail cause no valid email format", () => {
 				userCreation.email = "email.com";
+
 				expect(() => {service.validateUserCreation(userCreation)})
 				.to.throw(Unauthorized, "Invalid email format!");
 			});
 		});
-		describe("password property", () => {	
-			const userCreation: UserCreation = {
-				username: "Karhal",
-				email: "karhal@email.com",
-				password: ""
-			}
 
+		describe("password property", () => {	
 			it("should fail cause empty password", () => {
+				userCreation.password = "";
+
 				expect(() => {service.validateUserCreation(userCreation)})
 				.to.throw(Unauthorized, "Missing credentials!");
 			});
 
 			it("should fail cause too short (less then 6 symbols)", () => {
 				userCreation.password = "fds";
+
 				expect(() => {service.validateUserCreation(userCreation)})
 				.to.throw(Unauthorized, "Week password!");
 			});
 
 			it("should fail cause too long (more then 20 symbols)", () => {
 				userCreation.password = "sdfasdfdasfasdfdasffsgfhsgththrthggfhnn";
+
 				expect(() => {service.validateUserCreation(userCreation)})
 				.to.throw(Unauthorized, "Week password!");
 			});
 
 			it("should fail cause no digits", () => {
 				userCreation.password = "jfdadfFFjdk";
+
 				expect(() => {service.validateUserCreation(userCreation)})
 				.to.throw(Unauthorized, "Week password!");
 			});
 
 			it("should fail cause no lower-case symbols", () => {
 				userCreation.password = "464FDEDVA";
+
 				expect(() => {service.validateUserCreation(userCreation)})
 				.to.throw(Unauthorized, "Week password!");
 			});
 
 			it("should fail cause no capital symbols", () => {
 				userCreation.password = "464dfsde";
+				
 				expect(() => {service.validateUserCreation(userCreation)})
 				.to.throw(Unauthorized, "Week password!");
 			});
 		});
-  });
+	});
 });
