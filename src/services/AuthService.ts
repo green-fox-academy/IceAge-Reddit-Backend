40e6915a31fbec553999f74400179a342f339a81
Martin Fučík <mar.fucik@gmail.com>
2020-12-01 22:35:44 +0100

    Use env. var as string for TS compiling reasons

--- src/services/AuthService.ts
+++ src/services/AuthService.ts
@@ -7,7 +7,7 @@ import { JWToken } from '../models/auth.types';
 export class AuthService {
 	public getToken(username: string): JWToken {
 		const token: JWToken = {
-			token: jwt.sign(username, process.env.TOKEN_SECRET)
+			token: jwt.sign(username, process.env.TOKEN_SECRET as string)
 		}
 		return token;
 	}
