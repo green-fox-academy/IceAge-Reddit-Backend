fe8ea3b35db63a1ed6a8f28b9a0a5c98557c91c8
Andrea Malinova <an.malinova@seznam.cz>
2020-12-04 14:57:49 +0100

    need to pull

--- src/index.ts
+++ src/index.ts
@@ -2,9 +2,28 @@ import './preStart'; // Must be the first import
 import app from '@server';
 import logger from '@shared/Logger';
 
+import {createConnection} from "typeorm";
+
 
 // Start the server
 const port = Number(process.env.PORT || 3000);
 app.listen(port, () => {
     logger.info('Express server started on port: ' + port);
 });
+
+//connect to database
+createConnection({
+    type: "mysql",
+    host: "localhost",
+    port: 3306,
+    username: process.env.DATASOURCE_USERNAME,
+    password: process.env.DATASOURCE_PASSWORD,
+    database: "reddit",
+    entities: [
+        
+    ],
+    synchronize: true,
+    logging: false
+  }).then(connection => {
+    // here you can start to work with your entities
+  }).catch(error => console.log(error));
