835ee0732f8133ce3e40210ed5d97b1185142aee
Martin Fučík <mar.fucik@gmail.com>
2020-12-07 21:34:51 +0100

    Prolong token on successful verification

--- src/services/AuthService.ts
+++ src/services/AuthService.ts
@@ -3,16 +3,28 @@ import * as jwt from 'jsonwebtoken';
 
 import { JWToken } from '../models/auth.types';
 
+interface Email {
+	email: string;
+}
+
 @Service()
 export class AuthService {
+
+	private static TOKEN_EXPIRATION = '24h';
+
 	public getToken(email: string): JWToken {
 		const token: JWToken = {
-			token: jwt.sign(email, process.env.TOKEN_SECRET as string, { expiresIn: '24h' })
+			token: jwt.sign(
+				{ email: email },
+				process.env.TOKEN_SECRET as string,
+				{ expiresIn: AuthService.TOKEN_EXPIRATION }
+			)
 		}
 		return token;
 	}
 
-	public verifyToken(token: string): void {
-		jwt.verify(token, process.env.TOKEN_SECRET as string)
+	public verifyAndProlongToken(token: string): string {
+		const payload: Email = jwt.verify(token, process.env.TOKEN_SECRET as string) as Email;
+		return this.getToken(payload.email).token;
 	}
 }
