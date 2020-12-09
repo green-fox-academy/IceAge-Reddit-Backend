c54526d69a501ab4ae1c6e7ddf923e6283597cb3
Martin Fučík <mar.fucik@gmail.com>
2020-12-09 22:51:32 +0100

    Added tests for AuthService

--- tests/services/AuthService.spec.ts
+++ tests/services/AuthService.spec.ts
@@ -1,13 +1,22 @@
 /* eslint-disable @typescript-eslint/unbound-method */
 import { assert } from "chai";
+import { JWToken } from '../../src/models/auth.types';
 import { AuthService } from '../../src/services/AuthService';
 
 describe("AuthService", () => {
-	const service = new AuthService();
+	const service: AuthService = new AuthService();
+	let email: string;
+	beforeEach(() => {
+		email = 'karhal@gmail.com';
+	})
 	describe("getToken()", () => {
-		it("should not fail", () => {
-			const email = 'karhal@gmail.com';	
+		it("should not fail", () => {	
 			assert.doesNotThrow(() => {service.getToken(email)});
+		});
+
+		it("should return token", () => {
+			const token = service.getToken(email);
+			assert.hasAllKeys(token, ['token']);
 		});
 	});
 });
