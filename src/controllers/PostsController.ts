fd00d526fc7f4cb4d349d8fb8a8dd6f8abd059d7
Jirinka <pousteckajir@seznam.cz>
2021-01-18 21:05:00 +0100

    PostController fixed after review

--- src/controllers/PostsController.ts
+++ src/controllers/PostsController.ts
@@ -1,15 +1,11 @@
 import { BodyParams, Controller, Get, PathParams, Post, UseBefore } from '@tsed/common';
 import { Posts } from '../entities/Posts';
-import { Subreddits } from '../entities/Subreddits';
 import { AuthMiddleware } from '../middlewares/AuthMiddleware';
-
 import { PostsService } from '../services/PostsService';
-import { SubredditsService } from '../services/SubredditsService';
-
 
 @Controller('/')
-export class UserController {
-	constructor(private postsService: PostsService, private subredditsService: SubredditsService) {}
+export class PostController {
+	constructor(private postsService: PostsService) {}
 
 	@Get('/feed')
 	@UseBefore(AuthMiddleware)
@@ -17,11 +13,6 @@ export class UserController {
 		return this.postsService.findAll();
 	}
 
-	@Get('/subreddits')
-	@UseBefore(AuthMiddleware)
-	getSubreddits(): Promise<Subreddits[]> {
-		return this.subredditsService.findAll();
-	}
 	@Get("/feed/r/:name")
 	@UseBefore(AuthMiddleware)
 	findOne(
@@ -29,13 +20,12 @@ export class UserController {
 			return this.postsService.findByName(name);
 		}
 		
-		//Jirinka
-		@Post('/subreddits/posts/create')
-		@UseBefore(AuthMiddleware)
-		public async createPost(
-			@BodyParams()
-			post: Posts
-		): Promise<Posts> {
-			return this.postsService.create(post);
-		}	
+	@Post('/subreddits/posts/create')
+	@UseBefore(AuthMiddleware)
+	public async createPost(
+		@BodyParams()
+		post: Posts
+	): Promise<Posts> {
+		return this.postsService.create(post);
+	}	
 }
