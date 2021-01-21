ac33d00d3eff9d8811466195ade97c0c7d5dd789
TomasBenes <tomasbenes123gmail.com>
2021-01-21 22:10:21 +0100

    added functionality for possibility to create subreddit only if it does not exist

--- src/services/SubredditsService.ts
+++ src/services/SubredditsService.ts
@@ -1,4 +1,5 @@
 import { Service } from '@tsed/common';
+import { Unauthorized } from '@tsed/exceptions';
 import { Subreddits } from '../entities/Subreddits';
 import { SubredditsCreation } from '../models/auth.types';
 import { SubredditsRepository } from '../repositories/SubredditsRepository';
@@ -11,10 +12,30 @@ export class SubredditsService {
 	) {}
 
 	public async create(subreddits: SubredditsCreation): Promise<SubredditsCreation> {
+		if(await this.isAvailableSubredditName(subreddits.name)) {
+			if(!subreddits.description){
+				throw new Unauthorized('We want to see your awesome description!');
+			} 
+			if(!subreddits.title) {
+				throw new Unauthorized('Tell others also the title of your subreddit!');
+			}	
+			
+			if (!subreddits.name){
+				throw new Unauthorized('Include also the subreddit tag!');
+			}
+		}
 		return await this.subredditsRepository.save(subreddits);
 	} 
 	
 	public async findAll(): Promise <Subreddits[]> {
 		return await this.subredditsRepository.find();
+	}
+
+	private async isAvailableSubredditName(name: string): Promise<boolean> {
+		if (await this.subredditsRepository.findBySubredditName(name) == undefined) {
+			return true;
+		} else {
+			throw new Unauthorized("Subreddit name already taken!");
+		}
 	}
 }
