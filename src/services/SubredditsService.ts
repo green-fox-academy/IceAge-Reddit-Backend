b95f614ad10f8e5f7889a80a52e93b7341f87bf3
TomasBenes <tomasbenes123gmail.com>
2021-01-26 21:36:05 +0100

    added the more appropriate errors

--- src/services/SubredditsService.ts
+++ src/services/SubredditsService.ts
@@ -1,5 +1,5 @@
 import { Service } from '@tsed/common';
-import { Unauthorized } from '@tsed/exceptions';
+import { Conflict, UnprocessableEntity } from '@tsed/exceptions';
 import { Subreddits } from '../entities/Subreddits';
 import { SubredditsCreation } from '../models/auth.types';
 import { SubredditsRepository } from '../repositories/SubredditsRepository';
@@ -14,14 +14,14 @@ export class SubredditsService {
 	public async create(subreddits: SubredditsCreation): Promise<SubredditsCreation> {
 		if(await this.isAvailableSubredditName(subreddits.name)) {
 			if(!subreddits.description){
-				throw new Unauthorized('We want to see your awesome description!');
+				throw new UnprocessableEntity('We want to see your awesome description!');
 			} 
 			if(!subreddits.title) {
-				throw new Unauthorized('Tell others also the title of your subreddit!');
+				throw new UnprocessableEntity('Tell others also the title of your subreddit!');
 			}	
 			
 			if (!subreddits.name){
-				throw new Unauthorized('Include also the subreddit tag!');
+				throw new UnprocessableEntity('Include also the subreddit tag!');
 			}
 		}
 		return await this.subredditsRepository.save(subreddits);
@@ -35,7 +35,7 @@ export class SubredditsService {
 		if (await this.subredditsRepository.findBySubredditName(name) == undefined) {
 			return true;
 		} else {
-			throw new Unauthorized("Subreddit name already taken!");
+			throw new Conflict("Subreddit name already taken!");
 		}
 	}
 }
